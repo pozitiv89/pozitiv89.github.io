90a47b4f30a852e0166a11140064564f7f9e7bd2
pozitiv89 <melnikow32@mail.ru>
2017-05-21 18:05:07 +0300

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://diplom/admin/');
-define('HTTP_CATALOG', 'http://diplom/');
+define('HTTP_SERVER', 'http://pozitiv89.github.io/admin/');
+define('HTTP_CATALOG', 'http://pozitiv89.github.io/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://diplom/admin/');
-define('HTTPS_CATALOG', 'http://diplom/');
+define('HTTPS_SERVER', 'http://pozitiv89.github.io/admin/');
+define('HTTPS_CATALOG', 'http://pozitiv89.github.io/');
 
 // DIR
-define('DIR_APPLICATION', 'E:/OpenServer/OpenServer/domains/diplom/admin/');
-define('DIR_SYSTEM', 'E:/OpenServer/OpenServer/domains/diplom/system/');
-define('DIR_IMAGE', 'E:/OpenServer/OpenServer/domains/diplom/image/');
-define('DIR_LANGUAGE', 'E:/OpenServer/OpenServer/domains/diplom/admin/language/');
-define('DIR_TEMPLATE', 'E:/OpenServer/OpenServer/domains/diplom/admin/view/template/');
-define('DIR_CONFIG', 'E:/OpenServer/OpenServer/domains/diplom/system/config/');
-define('DIR_CACHE', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/cache/');
-define('DIR_DOWNLOAD', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/download/');
-define('DIR_LOGS', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/logs/');
-define('DIR_MODIFICATION', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/modification/');
-define('DIR_UPLOAD', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/upload/');
-define('DIR_CATALOG', 'E:/OpenServer/OpenServer/domains/diplom/catalog/');
+define('DIR_APPLICATION', 'pozitiv89.github.io/admin/');
+define('DIR_SYSTEM', 'pozitiv89.github.io/system/');
+define('DIR_IMAGE', 'pozitiv89.github.io/image/');
+define('DIR_LANGUAGE', 'pozitiv89.github.io/admin/language/');
+define('DIR_TEMPLATE', 'pozitiv89.github.io/admin/view/template/');
+define('DIR_CONFIG', 'pozitiv89.github.io/system/config/');
+define('DIR_CACHE', 'pozitiv89.github.io/system/storage/cache/');
+define('DIR_DOWNLOAD', 'pozitiv89.github.io/system/storage/download/');
+define('DIR_LOGS', 'pozitiv89.github.io/system/storage/logs/');
+define('DIR_MODIFICATION', 'pozitiv89.github.io/system/storage/modification/');
+define('DIR_UPLOAD', 'pozitiv89.github.io/system/storage/upload/');
+define('DIR_CATALOG', 'pozitiv89.github.io/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
