1e4c20c488c286cff95b8078d5a55bc1b868ac36
Богдан Мельников < >
2017-05-21 17:05:08 +0300

    commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://diplom/admin/');
+define('HTTP_CATALOG', 'http://diplom/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://diplom/admin/');
+define('HTTPS_CATALOG', 'http://diplom/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/OpenServer/domains/diplom/admin/');
+define('DIR_SYSTEM', 'E:/OpenServer/OpenServer/domains/diplom/system/');
+define('DIR_IMAGE', 'E:/OpenServer/OpenServer/domains/diplom/image/');
+define('DIR_LANGUAGE', 'E:/OpenServer/OpenServer/domains/diplom/admin/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/OpenServer/domains/diplom/admin/view/template/');
+define('DIR_CONFIG', 'E:/OpenServer/OpenServer/domains/diplom/system/config/');
+define('DIR_CACHE', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/upload/');
+define('DIR_CATALOG', 'E:/OpenServer/OpenServer/domains/diplom/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'auto');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://diplom/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://diplom/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/OpenServer/domains/diplom/catalog/');
+define('DIR_SYSTEM', 'E:/OpenServer/OpenServer/domains/diplom/system/');
+define('DIR_IMAGE', 'E:/OpenServer/OpenServer/domains/diplom/image/');
+define('DIR_LANGUAGE', 'E:/OpenServer/OpenServer/domains/diplom/catalog/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/OpenServer/domains/diplom/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/OpenServer/OpenServer/domains/diplom/system/config/');
+define('DIR_CACHE', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/OpenServer/domains/diplom/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'auto');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
